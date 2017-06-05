c351cf0d8983e989fcd0b4508d09b436b4420fcb
Hugo Dzin <humodz@hotmail.com>
2017-06-04 23:04:22 -0300

    add: draw a red cube

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,50 @@
+#include <GL/glut.h>
+
+
+
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+
+
+
+void onDisplay()
+{
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glColor3f(0.0f, 0.0f, 0.0f);
+	glutWireCube(1);
+
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glutSolidCube(1);
+
+	glutSwapBuffers();
+}
+
+
+
 int main(int argc, char **argv)
 {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+	glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+	glutInitWindowPosition(300, 100);
+	glutCreateWindow("Trabalho 3");
+
+	glEnable(GL_DEPTH_TEST);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(60.0f, 4.0f / 3.0f, 1.0f, 1000.0f);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    gluLookAt(2, 2, 2,    0, 0, 0,    0, 1 ,0);
+
+
+
+	glutDisplayFunc(onDisplay);
+	glutMainLoop();
 
 	return 0;
 }
